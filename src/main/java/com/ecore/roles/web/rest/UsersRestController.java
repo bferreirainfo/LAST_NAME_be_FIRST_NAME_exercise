88f219edbd179bcf22973d05c948bb116ec0b074
bferreirainfo <bferreira.info@gmail.com>
2023-05-13 18:52:48 -0300

    Fixing Resources Mapping

--- src/main/java/com/ecore/roles/web/rest/UsersRestController.java
+++ src/main/java/com/ecore/roles/web/rest/UsersRestController.java
@@ -1,20 +1,22 @@
 package com.ecore.roles.web.rest;
 
-import com.ecore.roles.service.UsersService;
-import com.ecore.roles.web.UsersApi;
-import com.ecore.roles.web.dto.UserDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import static com.ecore.roles.web.dto.UserDto.fromModel;
 
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-import static com.ecore.roles.web.dto.UserDto.fromModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecore.roles.service.UsersService;
+import com.ecore.roles.web.UsersApi;
+import com.ecore.roles.web.dto.UserDto;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,7 +26,7 @@ public class UsersRestController implements UsersApi {
     private final UsersService usersService;
 
     @Override
-    @PostMapping(
+    @GetMapping(
             produces = {"application/json"})
     public ResponseEntity<List<UserDto>> getUsers() {
         return ResponseEntity
@@ -35,7 +37,7 @@ public class UsersRestController implements UsersApi {
     }
 
     @Override
-    @PostMapping(
+    @GetMapping(
             path = "/{userId}",
             produces = {"application/json"})
     public ResponseEntity<UserDto> getUser(
