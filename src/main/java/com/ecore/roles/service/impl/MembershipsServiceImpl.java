93e42b71d4123d05f45510f1d6bcf200bc4816c8
bferreirainfo <bferreira.info@gmail.com>
2023-05-13 19:12:17 -0300

    Refactoring constructor injection to lombok style = less code.

--- src/main/java/com/ecore/roles/service/impl/MembershipsServiceImpl.java
+++ src/main/java/com/ecore/roles/service/impl/MembershipsServiceImpl.java
@@ -1,5 +1,12 @@
 package com.ecore.roles.service.impl;
 
+import static java.util.Optional.ofNullable;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
 import com.ecore.roles.exception.InvalidArgumentException;
 import com.ecore.roles.exception.ResourceExistsException;
 import com.ecore.roles.exception.ResourceNotFoundException;
@@ -8,30 +15,18 @@ import com.ecore.roles.model.Role;
 import com.ecore.roles.repository.MembershipRepository;
 import com.ecore.roles.repository.RoleRepository;
 import com.ecore.roles.service.MembershipsService;
+
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.UUID;
-
-import static java.util.Optional.ofNullable;
 
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class MembershipsServiceImpl implements MembershipsService {
 
     private final MembershipRepository membershipRepository;
     private final RoleRepository roleRepository;
-
-    @Autowired
-    public MembershipsServiceImpl(
-            MembershipRepository membershipRepository,
-            RoleRepository roleRepository) {
-        this.membershipRepository = membershipRepository;
-        this.roleRepository = roleRepository;
-    }
 
     @Override
     public Membership assignRoleToMembership(@NonNull Membership m) {
--- src/main/java/com/ecore/roles/service/impl/UsersServiceImpl.java
+++ src/main/java/com/ecore/roles/service/impl/UsersServiceImpl.java
@@ -1,23 +1,21 @@
 package com.ecore.roles.service.impl;
 
-import com.ecore.roles.client.UsersClient;
-import com.ecore.roles.client.model.User;
-import com.ecore.roles.service.UsersService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.stereotype.Service;
+
+import com.ecore.roles.client.UsersClient;
+import com.ecore.roles.client.model.User;
+import com.ecore.roles.service.UsersService;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class UsersServiceImpl implements UsersService {
 
     private final UsersClient usersClient;
-
-    @Autowired
-    public UsersServiceImpl(UsersClient usersClient) {
-        this.usersClient = usersClient;
-    }
 
     public User getUser(UUID id) {
         return usersClient.getUser(id).getBody();
