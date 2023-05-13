b2cb68130a368b3432a5090c43a8302c28a1e83d
bferreirainfo <bferreira.info@gmail.com>
2023-05-13 19:56:04 -0300

    removing more unused services and refactoring variable names

--- src/main/java/com/ecore/roles/service/impl/RolesServiceImpl.java
+++ src/main/java/com/ecore/roles/service/impl/RolesServiceImpl.java
@@ -8,14 +8,11 @@ import org.springframework.stereotype.Service;
 import com.ecore.roles.exception.ResourceExistsException;
 import com.ecore.roles.exception.ResourceNotFoundException;
 import com.ecore.roles.model.Role;
-import com.ecore.roles.repository.MembershipRepository;
 import com.ecore.roles.repository.RoleRepository;
-import com.ecore.roles.service.MembershipsService;
 import com.ecore.roles.service.RolesService;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 
 @Service
 @RequiredArgsConstructor
@@ -24,21 +21,19 @@ public class RolesServiceImpl implements RolesService {
     public static final String DEFAULT_ROLE = "Developer";
 
     private final RoleRepository roleRepository;
-    private final MembershipRepository membershipRepository;
-    private final MembershipsService membershipsService;
 
     @Override
-    public Role CreateRole(@NonNull Role r) {
-        if (roleRepository.findByName(r.getName()).isPresent()) {
+    public Role CreateRole(@NonNull Role role) {
+        if (roleRepository.findByName(role.getName()).isPresent()) {
             throw new ResourceExistsException(Role.class);
         }
-        return roleRepository.save(r);
+        return roleRepository.save(role);
     }
 
     @Override
-    public Role GetRole(@NonNull UUID rid) {
-        return roleRepository.findById(rid)
-                .orElseThrow(() -> new ResourceNotFoundException(Role.class, rid));
+    public Role GetRole(@NonNull UUID roleID) {
+        return roleRepository.findById(roleID)
+                .orElseThrow(() -> new ResourceNotFoundException(Role.class, roleID));
     }
 
     @Override
