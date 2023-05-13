5be194550fa090ac25151d031f8aa0bb10786b05
bferreirainfo <bferreira.info@gmail.com>
2023-05-13 19:42:10 -0300

    Fixing and refactoring MembershipsServiceImpl
    Changes:
    
    1: Load and assign Role to "savedMemberShip" so the "save" method works actually take effect.
    2: Remove unnecessary log annotation.
    3: rename rId to RoleID and M to Membership
    4: use the correct exception "ResourceNotFoundException" instead of "ResourceExistsException" when membership is not found

--- src/main/java/com/ecore/roles/service/impl/MembershipsServiceImpl.java
+++ src/main/java/com/ecore/roles/service/impl/MembershipsServiceImpl.java
@@ -8,7 +8,6 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 
 import com.ecore.roles.exception.InvalidArgumentException;
-import com.ecore.roles.exception.ResourceExistsException;
 import com.ecore.roles.exception.ResourceNotFoundException;
 import com.ecore.roles.model.Membership;
 import com.ecore.roles.model.Role;
@@ -18,33 +17,30 @@ import com.ecore.roles.service.MembershipsService;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 
-@Log4j2
 @Service
 @RequiredArgsConstructor
 public class MembershipsServiceImpl implements MembershipsService {
 
     private final MembershipRepository membershipRepository;
     private final RoleRepository roleRepository;
-
+    
     @Override
-    public Membership assignRoleToMembership(@NonNull Membership m) {
-
-        UUID roleId = ofNullable(m.getRole()).map(Role::getId)
+    public Membership assignRoleToMembership(@NonNull Membership membership) {
+        UUID roleId = ofNullable(membership.getRole()).map(Role::getId)
                 .orElseThrow(() -> new InvalidArgumentException(Role.class));
 
-        if (membershipRepository.findByUserIdAndTeamId(m.getUserId(), m.getTeamId())
-                .isPresent()) {
-            throw new ResourceExistsException(Membership.class);
-        }
-
-        roleRepository.findById(roleId).orElseThrow(() -> new ResourceNotFoundException(Role.class, roleId));
-        return membershipRepository.save(m);
+        Role role = roleRepository.findById(roleId).orElseThrow(() -> new ResourceNotFoundException(Role.class, roleId));
+        
+        Membership savedMembership = membershipRepository.findByUserIdAndTeamId(membership.getUserId(), membership.getTeamId())
+        												 .orElseThrow(() -> new ResourceNotFoundException(Membership.class, membership.getTeamId()));        
+        savedMembership.setRole(role);
+        
+        return membershipRepository.save(savedMembership);
     }
 
     @Override
-    public List<Membership> getMemberships(@NonNull UUID rid) {
-        return membershipRepository.findByRoleId(rid);
+    public List<Membership> getMemberships(@NonNull UUID roleID) {
+        return membershipRepository.findByRoleId(roleID);
     }
 }
