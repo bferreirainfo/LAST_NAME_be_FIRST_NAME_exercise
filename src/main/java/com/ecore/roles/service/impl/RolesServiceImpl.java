315fa998cd2757b0b56df61e1ca9a90acad7fab6
bferreirainfo <bferreira.info@gmail.com>
2023-05-13 19:52:29 -0300

    removing RolesServiceImpl from .gitignore

---
+++ src/main/java/com/ecore/roles/service/impl/RolesServiceImpl.java
@@ -0,0 +1,54 @@
+package com.ecore.roles.service.impl;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.ecore.roles.exception.ResourceExistsException;
+import com.ecore.roles.exception.ResourceNotFoundException;
+import com.ecore.roles.model.Role;
+import com.ecore.roles.repository.MembershipRepository;
+import com.ecore.roles.repository.RoleRepository;
+import com.ecore.roles.service.MembershipsService;
+import com.ecore.roles.service.RolesService;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+public class RolesServiceImpl implements RolesService {
+
+    public static final String DEFAULT_ROLE = "Developer";
+
+    private final RoleRepository roleRepository;
+    private final MembershipRepository membershipRepository;
+    private final MembershipsService membershipsService;
+
+    @Override
+    public Role CreateRole(@NonNull Role r) {
+        if (roleRepository.findByName(r.getName()).isPresent()) {
+            throw new ResourceExistsException(Role.class);
+        }
+        return roleRepository.save(r);
+    }
+
+    @Override
+    public Role GetRole(@NonNull UUID rid) {
+        return roleRepository.findById(rid)
+                .orElseThrow(() -> new ResourceNotFoundException(Role.class, rid));
+    }
+
+    @Override
+    public List<Role> GetRoles() {
+        return roleRepository.findAll();
+    }
+
+    private Role getDefaultRole() {
+        return roleRepository.findByName(DEFAULT_ROLE)
+                .orElseThrow(() -> new IllegalStateException("Default role is not configured"));
+    }
+}
