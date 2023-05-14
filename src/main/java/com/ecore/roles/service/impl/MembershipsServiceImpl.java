8b1c814f1728b21e965ae44d1c7f300246e7dbae
bferreirainfo <bferreira.info@gmail.com>
2023-05-14 11:56:32 -0300

    Validate if team is present

--- src/main/java/com/ecore/roles/service/impl/MembershipsServiceImpl.java
+++ src/main/java/com/ecore/roles/service/impl/MembershipsServiceImpl.java
@@ -1,12 +1,17 @@
 package com.ecore.roles.service.impl;
 
+import static com.ecore.roles.constants.ValidationConstants.INVALID_MEMBERSHIP_OBJECT;
 import static java.util.Optional.ofNullable;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.ecore.roles.client.model.Team;
+import com.ecore.roles.constants.ValidationConstants;
 import com.ecore.roles.exception.InvalidArgumentException;
 import com.ecore.roles.exception.ResourceExistsException;
 import com.ecore.roles.exception.ResourceNotFoundException;
@@ -15,6 +20,7 @@ import com.ecore.roles.model.Role;
 import com.ecore.roles.repository.MembershipRepository;
 import com.ecore.roles.repository.RoleRepository;
 import com.ecore.roles.service.MembershipsService;
+import com.ecore.roles.service.TeamsService;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -22,9 +28,10 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class MembershipsServiceImpl implements MembershipsService {
-
-    private final MembershipRepository membershipRepository;
+	
+	private final MembershipRepository membershipRepository;
     private final RoleRepository roleRepository;
+    private final TeamsService teamsService;
 
     @Override
     public Membership assignRoleToMembership(@NonNull Membership membership) {
@@ -37,6 +44,15 @@ public class MembershipsServiceImpl implements MembershipsService {
           }
 
           roleRepository.findById(roleId).orElseThrow(() -> new ResourceNotFoundException(Role.class, roleId));
+          
+          Team team = ofNullable(teamsService.getTeam(membership.getTeamId()))
+        		  		.orElseThrow(()-> new ResourceNotFoundException(Team.class, membership.getTeamId()));
+          List<UUID> teamMemberIds = ofNullable(team.getTeamMemberIds()).orElse(new ArrayList<>());
+          
+          if(!teamMemberIds.contains(membership.getUserId())){
+          	  throw new InvalidArgumentException(INVALID_MEMBERSHIP_OBJECT);
+          }
+		
           return membershipRepository.save(membership);
     }
 
